e7aaa932fe8f31fa180de1564d1400ddffca6001
Andrew Casey <60218234+AndrewC20@users.noreply.github.com>
2021-05-11 11:23:56 +0100

    Part 6 Done Got busy on Part 7 and done without sending off Part 6 first

--- AndrewCasey_S00200841/MainWindow.xaml.cs
+++ AndrewCasey_S00200841/MainWindow.xaml.cs
@@ -20,9 +20,88 @@ namespace AndrewCasey_S00200841
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        List<Game> AllGames;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] genre = { "All", "PC", "Xbox", "PS", "Switch" };
+            cbx_Genre.ItemsSource = genre;
+
+            GameData db = new GameData();
+
+            var query = from g in db.Games
+                        select g;
+            AllGames = query.ToList();
+            lbx_Games.ItemsSource = AllGames;
+        }
+
+        private void lbx_Games_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Game selectedGame = lbx_Games.SelectedItem as Game;
+            if(selectedGame != null)
+            {
+                //img_GameImage.Source = new BitmapImage(new Uri(selectedGame.Game_Image, UriKind.Relative));               
+                //tbx_GameDetails.Text = $"{selectedGame.Description}";
+                tbx_GamePrice.Text = $"{selectedGame.Price:C}";
+                tblk_GameDetails.Text = $"{selectedGame.Description}";
+            }
+        }
+
+        // TODO : get working Correctly
+        // Could say that a game that has more than one Genre will cause an issue in futre so would need fixing
+        private void cxb_Genre_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedGenre = cbx_Genre.SelectedItem as string;
+
+            List<Game> filterList = new List<Game>();
+
+            switch (selectedGenre)
+            {
+                case "All":
+                    lbx_Games.ItemsSource = AllGames;
+                    break;
+
+                case "PC":
+                    foreach(Game game in AllGames)
+                    {
+                        if (game.Platform is "PC, Xbox, PS, Switch")
+                            filterList.Add(game);
+                    }
+                    lbx_Games.ItemsSource = filterList;
+                    break;
+
+                case "Xbox":
+                    foreach (Game game in AllGames)
+                    {
+                        if (game.Platform is "Xbox")
+                            filterList.Add(game);
+                    }
+                    lbx_Games.ItemsSource = filterList;
+                    break;
+
+                case "PS":
+                    foreach (Game game in AllGames)
+                    {
+                        if (game.Platform is "PS")
+                            filterList.Add(game);
+                    }
+                    lbx_Games.ItemsSource = filterList;
+                    break;
+
+                case "Switch":
+                    foreach (Game game in AllGames)
+                    {
+                        if (game.Platform is "Switch")
+                            filterList.Add(game);
+                    }
+                    lbx_Games.ItemsSource = filterList;
+                    break;
+            }
         }
     }
 }
